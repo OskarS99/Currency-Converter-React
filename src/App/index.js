aec5076c9ed5e7a0187658aaf3af35795a9fe1a2
Oskar Siry <oskarsiry99@gmail.com>
2023-04-14 15:37:58 +0200

    style components to app added

--- src/App/index.js
+++ src/App/index.js
@@ -1,33 +1,28 @@
-import { useState } from 'react';
-import './App.css';
-import { Form } from './Form';
-import { Clock } from './Clock';
-import { currencies } from './currencies';
+import { useState } from "react";
+import { Container } from "./styled.js";
+import { Form } from "./Form";
+import { Clock } from "./Clock";
+import { currencies } from "./currencies";
 
 function App() {
-    const [result, setResult] = useState();
+  const [result, setResult] = useState();
 
-    const calculateResult = (currency, amount) => {
-        const rate = currencies
-            .find(({ short }) => short === currency)
-            .rate;
+  const calculateResult = (currency, amount) => {
+    const rate = currencies.find(({ short }) => short === currency).rate;
 
-        setResult({
-            sourceAmount: +amount,
-            targetAmount: amount / rate,
-            currency,
-        })
-    }
+    setResult({
+      sourceAmount: +amount,
+      targetAmount: amount / rate,
+      currency,
+    });
+  };
 
-    return (
-        <div className="container">
-            <Clock />
-            <Form
-                result={result}
-                calculateResult={calculateResult}
-            />
-        </div>
-    )
+  return (
+    <Container>
+      <Clock />
+      <Form result={result} calculateResult={calculateResult} />
+    </Container>
+  );
 }
 
 export default App;
---
+++ src/App/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  flex-basis: 700px;
+  margin: 20px;
+  padding: 40px;
+  border-radius: 20px;
+  border: 3px solid white;
+`;
