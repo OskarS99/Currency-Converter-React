21af844c87c95444b179c6c8f416a134c2ae5c67
Oskar Siry <oskarsiry99@gmail.com>
2023-04-20 19:00:11 +0200

    transform result useState and function to Form component

--- src/App/index.js
+++ src/App/index.js
@@ -1,26 +1,13 @@
-import { useState } from "react";
 import { Container } from "./styled.js";
 import { Form } from "./Form";
 import { Clock } from "./Clock";
-import { currencies } from "./currencies";
 
 function App() {
-  const [result, setResult] = useState();
-
-  const calculateResult = (currency, amount) => {
-    const rate = currencies.find(({ short }) => short === currency).rate;
-
-    setResult({
-      sourceAmount: +amount,
-      targetAmount: amount / rate,
-      currency,
-    });
-  };
 
   return (
     <Container>
       <Clock />
-      <Form result={result} calculateResult={calculateResult} />
+      <Form/>
     </Container>
   );
 }
