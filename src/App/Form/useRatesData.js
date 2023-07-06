52a80991aa7209758309f32f19bf7777640ba2e2
Oskar Siry <oskarsiry99@gmail.com>
2023-07-06 13:21:51 +0200

    "base" parameter added to Api link

--- src/App/Form/useRatesData.js
+++ src/App/Form/useRatesData.js
@@ -1,35 +1,34 @@
 import { useEffect, useState } from "react";
 
-
 export const useRatesData = () => {
-    const currencyApi = 'https://api.exchangerate.host/latest';
-    const [ratesData, setRatesData] = useState({
-        status: "loading",
-    });
+  const currencyApi = "https://api.exchangerate.host/latest?base=PLN";
+  const [ratesData, setRatesData] = useState({
+    status: "loading",
+  });
 
-    useEffect(() => {
-        const fetchRates = async () => {
-            try {
-                const response = await fetch(currencyApi);
+  useEffect(() => {
+    const fetchRates = async () => {
+      try {
+        const response = await fetch(currencyApi);
 
-                if (!response.ok){
-                    throw new Error(response.statusText);
-                }
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
 
-                const {rates, date} = await response.json();
-                
-                setRatesData({
-                    status: "success",
-                    rates,
-                    date,
-                });
-            } catch {
-                setRatesData({
-                    status: "error",
-                });
-            }
-        };
-        setTimeout(fetchRates, 1000);
-    }, []);
-    return ratesData;
-}
+        const { rates, date } = await response.json();
+
+        setRatesData({
+          status: "success",
+          rates,
+          date,
+        });
+      } catch {
+        setRatesData({
+          status: "error",
+        });
+      }
+    };
+    setTimeout(fetchRates, 1000);
+  }, []);
+  return ratesData;
+};
